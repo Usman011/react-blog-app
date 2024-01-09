03e9cd65918c7074773d1b0642122e701bd0de0d
Usman011 <m.usmannasir011@gmail.com>
2024-01-09 15:28:42 +0500

    adds eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+      helpers: '/src/helpers',
+      theme: '/src/theme',
+      store: '/src/store',
+      constants: '/src/constants',
+      api: '/src/api'
+    }
+  }
 })
